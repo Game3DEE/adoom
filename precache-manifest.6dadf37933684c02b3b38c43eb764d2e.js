dc9a5d87c2438840f86e14c1d44fc5202de47f62
Ithamar <ithamar@upgrade-android.com>
2020-12-19 10:22:13 +0000

    Deploying to gh-pages from  @ ca9b27f246059ef7ab62a5a940cd570def58c0c0 🚀

---
+++ precache-manifest.6dadf37933684c02b3b38c43eb764d2e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5594f1ac44d16af4a55877794ac467dd",
+    "url": "assets/.DS_Store"
+  },
+  {
+    "revision": "c6465e4fc058915dd6da531aa722bebc",
+    "url": "assets/ammo/ammo.js"
+  },
+  {
+    "revision": "b9fad0a49e64f4fa99fe43aa1fb85de3",
+    "url": "assets/ammo/ammo.wasm.js"
+  },
+  {
+    "revision": "8640fe8d5bf91d2e84ea56e0c1307ba9",
+    "url": "assets/ammo/ammo.wasm.wasm"
+  },
+  {
+    "revision": "b2649ec9cfb9839c7e525b85999845b8",
+    "url": "assets/img/HUDFont.png"
+  },
+  {
+    "revision": "7edf14cbbf2bf6ad0f34e855a0cc5698",
+    "url": "assets/img/HUDFont2.png"
+  },
+  {
+    "revision": "17d3ab1c6e8ceebd62cef51a5ccfa32e",
+    "url": "assets/img/HUDIcons.png"
+  },
+  {
+    "revision": "ec0a2852ce8f35e69bc6d296e4d3fa1c",
+    "url": "assets/img/HUDIcons2.png"
+  },
+  {
+    "revision": "e99893f3418e3ef7dc94735451b3e0a3",
+    "url": "assets/levels/level1_1.bsp"
+  },
+  {
+    "revision": "8da381eff93ea07f15121128d009a009",
+    "url": "assets/levels/level1_1.wps"
+  },
+  {
+    "revision": "e69617b99aeb79c524a40e3af545b5f0",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "0f8a97f167330666608c779f22c3919d",
+    "url": "index.html"
+  },
+  {
+    "revision": "4f2f49ab28d779dca44a",
+    "url": "main.df23bbdf27b5b876aacf.bundle.js"
+  },
+  {
+    "revision": "bf38d0c9760162342f12cd9b44fbf4ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "da88053f5ad340ca88e8",
+    "url": "vendors.fdf6ebab51574ccb1456.bundle.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,11 @@
+importScripts("precache-manifest.6dadf37933684c02b3b38c43eb764d2e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
